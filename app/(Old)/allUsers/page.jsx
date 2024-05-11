8dafce4cb51d53fd8e10129e97e37e37ed83480c
ADITYA JHA <a4adityajha@gmail.com>
2024-05-11 11:05:25 +0530

    styled all users page

--- app/(Old)/allUsers/page.jsx
+++ app/(Old)/allUsers/page.jsx
@@ -1,8 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import { ThemeProvider, useTheme } from 'next-themes';
-import styles from './allUsers.module.css'
+import { ThemeProvider, useTheme } from "next-themes";
+import styles from "./allUsers.module.css";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function AllUser() {
   const [users, setUsers] = useState([]);
@@ -14,7 +16,7 @@ export default function AllUser() {
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const res = await fetch('http://localhost:3000/api/user');
+        const res = await fetch("http://localhost:3000/api/user");
         if (!res.ok) {
           throw new Error("Failed to fetch users");
         }
@@ -24,33 +26,56 @@ export default function AllUser() {
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     fetchUser();
   }, []);
 
   // Filter out the current user and the specific user by email
-  const filteredUsers = users.filter(user => user.email !== "ayushjha5467@gmail.com" && user.id !== localStorage.getItem("userId"));
+  const filteredUsers = users.filter(
+    (user) =>
+      user.email !== "ayushjha5467@gmail.com" &&
+      user.id !== localStorage.getItem("userId")
+  );
 
   return (
     <ThemeProvider>
       {/* <div className={styles.center}>
         <h1 className={styles.heading}>All Users</h1>
         <div className={styles.main}>  */}
-        <div className="max-w-[912px] px-3 mx-auto mt-4">
+      <div className="max-w-[912px] px-3 mx-auto mt-4 m-7">
+        <div>
+          <h1 className="text-2xl font-bold text-neutral-700 mt-2">All Users</h1>
+        </div>
         <div className="flex justify-between items-center m-7">
-          <h1 className="text-2xl font-bold text-neutral-700">
-            Coding Courses
-          </h1>
-          {filteredUsers.map((user, index) => (
-            <div className={`${styles.card} ${resolvedTheme === 'dark' ? styles.darkCard : styles.lightCard}`} key={index}>
-              <p><span className={styles.name}>Name:</span>  {user.name}</p>
-              <p> <span className={styles.name}>Email:</span> {user.email}</p>
-              <p> <span className={styles.name}>Badge: </span>{user.badge}</p>
-            </div>
-          ))}
+          <div>
+            {filteredUsers.map((user, index) => (
+              
+              <div
+                className={`${styles.card} ${
+                  resolvedTheme === "dark" ? styles.darkCard : styles.lightCard
+                }`}
+                key={index}
+              >
+              <div className="flex-col justify-normal items-center">
+              <Image src="/smartUser.jpg" alt="user" height={50} width={50} className="rounded-full " />
+                <p className="uppercase ">
+                  <span className={styles.name}>Name:</span> {user.name}
+                </p>
+                <p className="uppercase ">
+                  {" "}
+                  <span className={styles.name}>Email:</span> {user.email}
+                </p>
+                <p className="uppercase ">
+                  {" "}
+                  <span className={styles.name}>Badge: </span>
+                  {user.badge}
+                </p>
+              </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </ThemeProvider>
-
   );
 }
